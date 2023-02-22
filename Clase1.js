7fa3924858e3a248e58a187a703ea439816a83ed
MassiSulic <massimo.a.sulic@gmail.com>
2023-02-22 20:22:33 -0300

    Primer Commit y ejercicios de la primera clase

---
+++ Clase1.js
@@ -0,0 +1,48 @@
+// Clase 1- “Introducción a la Programación Web”
+
+// 1) Declarar una variable de nombre “auto”, asignarle el valor “Corolla” y mostrarla por consola.
+
+var auto = "Corolla"
+
+console.log(auto);
+
+// 2) Asignarle el valor “308” a la variable declarada en el ejercicio anterior y mostrarla por consola.
+
+auto = 308
+
+console.log(auto);
+
+// 3) A la misma variable de los ejercicios anteriores, asignarle el valor “C4”. Volver a mostrarla por consola.
+
+auto = "C4"
+
+console.log(auto);
+
+// 4) Declarar dos variables “nombre” y “apellido”, asignarles tu nombre y apellido. Luego mostrar por consola ambos valores. Usar un solo console.log( )
+
+var Nombre = "Massimo"
+var Apellido = "Sulic"
+
+console.log(Nombre, Apellido);
+
+// 5) Dada una variable llamada “fruta” con el valor “naranja”. Declarar una variable “tipoDeLaVariableFruta” y asignarle como valor el tipo de la variable “fruta”.
+
+var Fruta = "Naranja"
+
+var tipoDeLaVariableFruta = typeof(Fruta)
+
+console.log(tipoDeLaVariableFruta);
+
+// 6) Para los ejercicios 1, 2 y 3, mostrar por consola el tipo de la variable en cada nueva asignación.
+
+var auto = "Corolla"
+
+console.log(auto, typeof(auto));
+
+auto = 308
+
+console.log(auto, typeof(auto));
+
+auto = "C4"
+
+console.log(auto, typeof(auto));
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ArgentinaPrograma
-Todos los ejercicios de Argentina Programa 4.0
+
+Entre a Argentina Programa 4.0 y acaban de empezar las clases, asi que voy a subir todos los ejercicios que vayan dando. 
+
+## Objetivos
+
+- Todos los ejercicios de Argentina Programa 4.0
