c10a22cefda64e16c8e3037968134a36c1bfb5ee
MassiSulic <massimo.a.sulic@gmail.com>
2023-03-01 20:09:35 -0300

    Modificaciones

---
+++ Clase3.js
@@ -0,0 +1,10 @@
+/* 1) Generar una función que le ingrese por parámetro algún dato ya sea ingresado por el usuario 
+o declarado anteriormente en su código y mostrar por consola el tipo de dato que es. */
+
+function tipoDeDato (dato) {
+    console.log(typeof(dato));
+}
+
+tipoDeDato(12)
+
+// 
